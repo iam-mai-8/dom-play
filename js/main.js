d41e83562d1aade498c9af6dbfc421c6138c6618
Maira <maira.yanez@seattlecolleges.edu>
2022-12-15 21:04:32 -0800

    JS updated

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
 
-function highlight(el){
-
+function highlight(el) {
     //el.style.backgroundColor='blue';
 
-    if(el.style.backgroundColor =='black'){ //black turn to yellow
-        el.style.backgroundColor='green';
-    }else{//turn to black
-        el.style.backgroundColor='black';
+    if(el.style.backgroundColor =='white'){ //white turn to yellow
+        el.style.backgroundColor='yellow';
+    }else{//turn to white
+        el.style.backgroundColor='white';
     }
 }
   
   function myAlert(){
-    alert("I'm clicked!")
+    alert("I'm clicked!");
+  }
+
+  function highlightActor(actor){
+    let spans = document.querySelectorAll("#play span");
+    for(const mySpan of spans){
+        if (actor == mySpan.dataset.actor){//white turn to yellow
+            mySpan.style.backgroundColor='yellow';
+        }else {//turn to white
+            mySpan.style.backgroundColor='white';
+
+    }
+    //alert(actor;
+}
   }
 
   let spans = document.querySelectorAll("#play span");
@@ -19,38 +31,15 @@ function highlight(el){
 console.log(spans);
 
 for(const mySpan of spans){
-    mySpan.addEventListener("click", myAlert);
+   // mySpan.addEventListener("click", myAlert);
+   //alert(mySpan.dataset.actor);
+
+   mySpan.addEventListener("click", function(ev){
+    highlightActor(mySpan.dataset.actor);
+
+   });
+
 }
   
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  //https://tinyurl.com/dynamic-html-checker
-  document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
-      
-  document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
-   
-   //set current year in span with id of this-year
-   let d = new Date(); let thisYear = d.getFullYear();
-   document.getElementById("this-year").innerHTML = thisYear;
-  
-  //manages mobile nav 
-  function myFunction() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
-          x.className += " responsive";
-      } else {
-          x.className = "topnav";
-      }
-  }   
